320296496df99fb5c1bf414b553c32d24ba151b6
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-11-20 01:10:18 +0700

    finishing frontpage

--- src/Pages/FrontPage/index.js
+++ src/Pages/FrontPage/index.js
@@ -1,11 +1,31 @@
 import React from "react";
+import MyProfile from "../../Component/MyProfile";
 import Navbar from "../../Component/Navbar/Navbar";
+import profilePicture from "../../Assets/img/profilePicture.png";
+
+import "./style.css";
+import About from "../../Component/About";
+import Bio from "../../Component/Bio";
+import Hobbies from "../../Component/Hobbies";
+import Contact from "../../Component/Contact";
+import Footer from "../../Component/Footer";
 
 function index() {
   return (
-    <div className="home-container">
+    <>
       <Navbar />
-    </div>
+      <div className="home-container">
+        <MyProfile
+          profilePict={profilePicture}
+          fullName="Fillah Akbar Firdausyah"
+        />
+        <About />
+        <Bio />
+        <Hobbies />
+        <Contact />
+        <Footer />
+      </div>
+    </>
   );
 }
 
