e81dfab3af58351840a6ce23e094efc867777c0d
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-11-20 01:08:12 +0700

    create works card component

---
+++ src/Component/WorksCard/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Wecan from "../../Assets/works/wecan.png";
+
+import "./style.css";
+
+function WorksCard(props) {
+  return (
+    <div className="works-card-wrapper">
+      <a href="">
+        <img src={Wecan} alt="" />
+      </a>
+      <a href="">
+        <h3>{props.title}</h3>
+      </a>
+      <p>{props.description}</p>
+    </div>
+  );
+}
+
+export default WorksCard;
