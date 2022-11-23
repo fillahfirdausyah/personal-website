f8912d5815649e7735a5f6ece87832d247738bf4
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-11-23 12:11:39 +0700

    make dynamic navbar active

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,21 +1,36 @@
 import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
+
 import "./style.css";
 
 function Navbar() {
+  const location = useLocation();
+  const { pathname } = location;
   return (
     <nav className="nav-container">
       <ul>
         <li>
-          <a href="/">Home</a>
+          <a href="/" className={pathname == "/" ? "active" : ""}>
+            Home
+          </a>
         </li>
         <li>
-          <a href="/works">Works</a>
+          <a href="/works" className={pathname == "/works" ? "active" : ""}>
+            Works
+          </a>
         </li>
         <li>
-          <a href="/techstacks">Tech Stack</a>
+          <a
+            href="/techstacks"
+            className={pathname == "/techstacks" ? "active" : ""}
+          >
+            Tech Stack
+          </a>
         </li>
         <li>
-          <a href="https://github.com/fillahfirdausyah/personal-website">Github</a>
+          <a href="https://github.com/fillahfirdausyah/personal-website">
+            Github
+          </a>
         </li>
       </ul>
     </nav>
