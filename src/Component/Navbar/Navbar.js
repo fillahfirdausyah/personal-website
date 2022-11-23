368a17799acd49410f292341bf628a9262a26cbe
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-11-23 20:07:50 +0700

    implement responsive navbar

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,39 +1,109 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
+
+import { BiMenu } from "react-icons/bi";
 
 import "./style.css";
 
 function Navbar() {
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+  const [isNavItemVisible, setIsNavItemVisible] = useState(false);
+
   const location = useLocation();
   const { pathname } = location;
+
+  const handleWindowSize = useCallback((event) => {
+    setWindowSize(window.innerWidth);
+  });
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSize);
+
+    return () => window.removeEventListener("resize", handleWindowSize);
+  }, [handleWindowSize]);
+
   return (
-    <nav className="nav-container">
-      <ul>
-        <li>
-          <a href="/" className={pathname == "/" ? "active" : ""}>
-            Home
-          </a>
-        </li>
-        <li>
-          <a href="/works" className={pathname == "/works" ? "active" : ""}>
-            Works
-          </a>
-        </li>
-        <li>
-          <a
-            href="/techstacks"
-            className={pathname == "/techstacks" ? "active" : ""}
+    <>
+      {windowSize > 576 ? (
+        <nav className="nav-container">
+          <ul>
+            <li>
+              <a href="/" className={pathname == "/" ? "active" : ""}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="/works" className={pathname == "/works" ? "active" : ""}>
+                Works
+              </a>
+            </li>
+            <li>
+              <a
+                href="/techstacks"
+                className={pathname == "/techstacks" ? "active" : ""}
+              >
+                Tech Stack
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/fillahfirdausyah/personal-website">
+                Github
+              </a>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <nav className="nav-mobile-container">
+          <div className="top-nav">
+            <div className="top-nav-item">
+              <h4>Fillah Firdausyah</h4>
+              <div
+                className="nav-icon-wrapper"
+                onClick={() => setIsNavItemVisible(!isNavItemVisible)}
+              >
+                <BiMenu size={"30px"} />
+              </div>
+            </div>
+          </div>
+          <div
+            className={
+              isNavItemVisible
+                ? "nav-mobile-item-container angry-animate"
+                : "nav-mobile-item-container hidden"
+            }
           >
-            Tech Stack
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/fillahfirdausyah/personal-website">
-            Github
-          </a>
-        </li>
-      </ul>
-    </nav>
+            <ul>
+              <li>
+                <a href="/" className={pathname == "/" ? "active" : ""}>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/works"
+                  className={pathname == "/works" ? "active" : ""}
+                >
+                  Works
+                </a>
+              </li>
+              <li>
+                <a
+                  href="/techstacks"
+                  className={pathname == "/techstacks" ? "active" : ""}
+                >
+                  Tech Stack
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/fillahfirdausyah/personal-website">
+                  Github
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      )}
+    </>
   );
 }
 
