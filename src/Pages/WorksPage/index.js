84b6650b137abbea6ab209db47b458e68a5287a3
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-11-23 20:07:27 +0700

    make loading state

--- src/Pages/WorksPage/index.js
+++ src/Pages/WorksPage/index.js
@@ -5,16 +5,20 @@ import supabaseClient from "../../api/supabaseClient";
 import Navbar from "../../Component/Navbar/Navbar";
 import WorksCard from "../../Component/WorksCard";
 import Footer from "../../Component/Footer";
+import Loading from "../../Component/Loading";
 
 import "./style.css";
 
 function WorksPage() {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fethcData = async () => {
+      setIsLoading(true);
       const { data, error } = await supabaseClient.from("works").select();
       setData(data);
+      setIsLoading(false);
     };
     fethcData();
   }, []);
@@ -29,17 +33,23 @@ function WorksPage() {
         </p>
         <div className="works-section-wrapper">
           <h3 className="section-title">Portfolio</h3>
-          <div className="list-works row">
-            {data.map((x) => (
-              <div key={x.id}>
-                <WorksCard
-                  cover={x.cover}
-                  title={x.title}
-                  description={x.short_desc}
-                />
-              </div>
-            ))}
-          </div>
+          {isLoading ? (
+            <div className="loading">
+              <Loading />
+            </div>
+          ) : (
+            <div className="list-works row">
+              {data.map((x) => (
+                <div key={x.id}>
+                  <WorksCard
+                    cover={x.cover}
+                    title={x.title}
+                    description={x.short_desc}
+                  />
+                </div>
+              ))}
+            </div>
+          )}
         </div>
         <Footer />
       </div>
