a6137e4fac64aa5ebd16d1a6e6a0d167c186c418
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-12-12 13:47:45 +0700

    implement auth using supabase

--- src/Pages/Auth/LoginPage/index.js
+++ src/Pages/Auth/LoginPage/index.js
@@ -1,23 +1,55 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import supabaseClient from "../../../api/supabaseClient";
 
 import "./style.css";
 
 function Login() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alertMessage, setAllertMessage] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      setIsLoading(true);
+      const { data, error } = await supabaseClient.auth.signInWithPassword({
+        email,
+        password,
+      });
+    } catch (error) {
+      setAllertMessage(error.error_description || error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="home-container login-page">
       <div className="login-wrapper">
         <h2>Login</h2>
-        {/* <p className="alert">some word to notify user</p> */}
-        <form>
+        {alertMessage == "" ? "" : <p className="alert">{alertMessage}</p>}
+        <form onSubmit={handleLogin}>
           <div className="item-input">
-            <h5>Username</h5>
-            <input type="text" />
+            <h5>Email</h5>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="item-input">
             <h5>Password</h5>
-            <input type="text" />
+            <input
+              value={password}
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
-          <div className="btn btn-login">Login</div>
+          <button type="submit" className="btn btn-login">
+            Login
+          </button>
         </form>
       </div>
     </div>
