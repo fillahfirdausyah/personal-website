672c95a00415e89a9febfc09d7791cc5b8747e5f
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-12-12 10:33:53 +0700

    add route

--- src/Component/WorksCard/index.js
+++ src/Component/WorksCard/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 
-import Wecan from "../../Assets/works/wecan.png";
-
 import "./style.css";
 
 function WorksCard(props) {
   return (
-    <div className="works-card-wrapper">
-      <a href="">
+    <div className="works-card-wrapper"> 
+      <a href={`/works/${props.title}`}>
         <img src={props.cover} alt="" />
       </a>
-      <a href="">
+      <a href={`/works/${props.title}`}>
         <h3>{props.title}</h3>
       </a>
       <p>{props.description}</p>
