3a75c1fe7d2e59001748e0b7a558b7a40b78a0eb
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2022-12-19 12:34:33 +0700

    make filter feature, to filter works

--- src/Pages/WorksPage/index.js
+++ src/Pages/WorksPage/index.js
@@ -7,12 +7,15 @@ import WorksCard from "../../Component/WorksCard";
 import Footer from "../../Component/Footer";
 import Loading from "../../Component/Loading";
 
+import { CiFilter } from "react-icons/ci";
+
 import "./style.css";
 import FadeIn from "react-fade-in/lib/FadeIn";
 
 function WorksPage() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const fethcData = async () => {
@@ -24,12 +27,51 @@ function WorksPage() {
     fethcData();
   }, []);
 
+  const filterData = async (category) => {
+    setIsLoading(true);
+    setIsVisible(false);
+    if (category == "all") {
+      const { data, error } = await supabaseClient.from("works").select();
+      setData(data);
+    } else {
+      const { data, error } = await supabaseClient
+        .from("works")
+        .select()
+        .eq("category", category);
+      setData(data);
+    }
+    setIsLoading(false);
+  };
+
   return (
     <>
       <Navbar />
       <div className="main-page-wrapper">
         <div className="works-section-wrapper">
-          <h3 className="section-title">My Works</h3>
+          <div className="top-works-section">
+            <h3 className="section-title">My Works</h3>
+            <div className="filter-works-wrapper">
+              <div
+                className="btn btn-filter"
+                onClick={() => setIsVisible(!isVisible)}
+              >
+                <CiFilter />
+              </div>
+              <div
+                className={
+                  isVisible
+                    ? "filter-value angry-animate"
+                    : "filter-value appear-out"
+                }
+              >
+                <ul>
+                  <li onClick={() => filterData("all")}>All</li>
+                  <li onClick={() => filterData("web")}>Web App</li>
+                  <li onClick={() => filterData("android")}>Android App</li>
+                </ul>
+              </div>
+            </div>
+          </div>
           {isLoading ? (
             <div className="loading">
               <Loading />
